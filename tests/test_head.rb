c0ed946fd09fd63922e9d039aa814cfc57c5294e
LoaTBaC <mason.hutchinson159@gmail.com>
2016-06-23 03:58:22 -0400

    Add files via upload

--- tests/test_head.rb
+++ tests/test_head.rb
@@ -3,30 +3,39 @@ require 'test/unit'
 
 class TestHead < Test::Unit::TestCase
   def test_init
-    t = Rbtm::Tape.new('0010')
-    h = Rbtm::Head.new(t, '0')
+    h = Rbtm::Head.new('', 'st0')
 
-    assert_equal(h.tape, t)
-    assert_equal(h.state, '0')
-    assert_equal(h.position, t.position)
+    assert_equal(h.index, 0)
+    assert_equal(h.state, 'st0')
+    assert_equal(h.tape, '_')
+
+    h = Rbtm::Head.new('0010', 'st0')
+
+    assert_equal(h.index, 0)
+    assert_equal(h.state, 'st0')
+    assert_equal(h.tape, '0010')
   end
 
   def test_operate
-    r = Rbtm::Rule.new('flop', '1', 'flop', '0', 'R')
-    t = Rbtm::Tape.new('10')
-    h = Rbtm::Head.new(t, 'flop')
+    h = Rbtm::Head.new('01', 'st0')
+    r = ['st1', '1', 'R']
 
     h.operate(r)
 
-    assert_equal(h.position, 1)
-    assert_equal(h.state, 'flop')
-    assert_equal(h.tape.to_s, '00')
+    assert_equal(h.index, 1)
+    assert_equal(h.state, 'st1')
+    assert_equal(h.tape, '11')
   end
 
   def test_signature
-    t = Rbtm::Tape.new
-    h = Rbtm::Head.new(t, 'done')
+    h = Rbtm::Head.new('10_000', 'done')
 
-    assert_equal(h.signature, ['done', '_'])
+    assert_equal(h.signature, ['done', '1'])
+  end
+
+  def test_to_s
+    h = Rbtm::Head.new('__1000_10__0_', 'flop')
+
+    assert_equal(h.to_s, '1000_10__0')
   end
 end
